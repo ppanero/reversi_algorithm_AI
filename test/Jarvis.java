4a77b5ff80861952e54fa99bb26521a732de39be
Satish Rajagopal <satishrajagopal.21@gmail.com>
2015-04-03 17:03:09 +0300

    Commiting so that the branch can be merged to master branch. Result may be wrong

--- test/Jarvis.java
+++ test/Jarvis.java
@@ -8,7 +8,7 @@ import java.util.Vector;
 public class Jarvis implements ReversiAlgorithm
 {
     // Constants
-    private final static int DEPTH_LIMIT = 6; // Just an example value.
+    private final static int DEPTH_LIMIT = 10; // Just an example value.
 
     // Variables
     boolean initialized;
@@ -45,8 +45,10 @@ public class Jarvis implements ReversiAlgorithm
         initialized = false;
         running = true;
         selectedMove = null;
-        //System.out.print("Player Index : "); System.out.println(myIndex);
+        // System.out.print("Player Index : "); System.out.println(myIndex);
+        System.out.println("---------------------");
         System.out.println("Jarvis searching ....");
+        System.out.println("---------------------");
 
         int currentDepth = 1;
 
@@ -99,11 +101,12 @@ public class Jarvis implements ReversiAlgorithm
     	
     	GameState n_state = n.getState();
     	int oppIndex, w, h, plrCount, oppCount;
-    	double coinparity_hvalue, mobility_hvalue, corner_hvalue, hvalue;
+    	double coinparity_hvalue, mobility_hvalue, position_hvalue, move_hvalue, hvalue;
     	oppIndex = (myIndex+1)%2;
     	
         if(currentDepth <= maxDepth)
         {
+        	System.out.print("Current Depth :"); System.out.println(currentDepth);
             Vector children = n_state.getPossibleMoves(player);
             if (children.size() > 0) 
             {
@@ -116,55 +119,75 @@ public class Jarvis implements ReversiAlgorithm
                     h = child_state.getHeight();
                     //System.out.print("Board size : "); System.out.print(w);System.out.println(h);
                     
-                    // Coin parity
-                    plrCount = child_state.getMarkCount(myIndex);
-                    oppCount = child_state.getMarkCount(oppIndex);
-                    coinparity_hvalue = (plrCount-oppCount) / (plrCount+oppCount);
+                    // Coin parity value
                     
-                    // Mobility value                    
-                    plrCount = child_state.getPossibleMoveCount(myIndex);
+                    /*plrCount = child_state.getMarkCount(myIndex);
+                    oppCount = child_state.getMarkCount(oppIndex);
+                    coinparity_hvalue = (plrCount-oppCount) / (plrCount+oppCount);*/
+                    
+                    // Mobility value    
+                    
+                    /*plrCount = child_state.getPossibleMoveCount(myIndex);
                     oppCount = child_state.getPossibleMoveCount(oppIndex);
                     if((plrCount+oppCount) != 0)
                     	mobility_hvalue = (plrCount-oppCount) / (plrCount+oppCount);
                     else
-                    	mobility_hvalue = 0;
+                    	mobility_hvalue = 0;*/
                     
-                    // Corner value     
-                   // System.out.println(child_state.getMarkAt(w, 0));
-                   // System.out.println(child_state.getMarkAt(0, h));
-                   // System.out.println(child_state.getMarkAt(w, 0));
-                    plrCount = 0;
-                    oppCount = 0;
-                    if(child_state.getMarkAt(0, 0) != -1)
-	                    if(child_state.getMarkAt(0, 0) == myIndex)
-	                    	plrCount++;
-	                    else
-	                    	oppCount++;
-                    if(child_state.getMarkAt(w-1, 0) != -1)
-    	                    if(child_state.getMarkAt(w, 0) == myIndex)
-    	                    	plrCount++;
-    	                    else
-    	                    	oppCount++;
-                   if(child_state.getMarkAt(0, h-1) != -1)
-    	                    if(child_state.getMarkAt(0, 0) == myIndex)
-    	                    	plrCount++;
-    	                    else
-    	                    	oppCount++;
-                    if(child_state.getMarkAt(w-1, h-1) != -1)
-    	                    if(child_state.getMarkAt(0, 0) == myIndex)
-    	                    	plrCount++;
-    	                    else
-    	                    	oppCount++;                    
-                    if((plrCount+oppCount) != 0)
-                    	corner_hvalue = (plrCount-oppCount) / (plrCount+oppCount);
-                    else
-                    	corner_hvalue = 0;
+                    // Positional value
                     
-                    hvalue = 0.01*coinparity_hvalue + 10.0*mobility_hvalue + 100000.0*corner_hvalue;
-                    System.out.println(hvalue);
-                    child.setScore(hvalue);
+                   /* position_hvalue = 0;
+                    int[][] position_wts = { {1000,-300,100,80,80,100,-300,1000},
+                    						 {-300,-500,-45,-50,-50,-45,-500,-300},
+                    						 {100,-45,3,1,1,3,-45,100},
+                    						 {80,-50,1,5,5,1,-50,80},
+                    						 {80,-50,1,5,5,1,-50,80},
+                    						 {100,-45,3,1,1,3,-45,100},
+                    						 {-300,-500,-45,-50,-50,-45,-500,-300},
+                    						 {1000,-300,100,80,80,100,-300,1000} };
+                    for(int i1=0;i1<w;i1++)
+                    {
+                    	for(int i2=0;i2<h;i2++)
+                    	{
+                    		//position_wts is symmetric
+                    		if(child_state.getMarkAt(i1,i2) != -1)
+                    		{
+                    			int sign = (child_state.getMarkAt(i1,i2) == myIndex) ? 1 : 0;
+                    			position_hvalue += sign * position_wts[i2][i1];
+                    		}
+                    	}
+                    }*/
+                    
+                    // Move value
+                    
+                    move_hvalue = 0;
+                    int[][] move_wts = { {1000,-300,100,80,80,100,-300,1000},
+                						 {-300,-500,-45,-50,-50,-45,-500,-300},
+                						 {100,-45,3,1,1,3,-45,100},
+                						 {80,-50,1,5,5,1,-50,80},
+                						 {80,-50,1,5,5,1,-50,80},
+                						 {100,-45,3,1,1,3,-45,100},
+                						 {-300,-500,-45,-50,-50,-45,-500,-300},
+                						 {1000,-300,100,80,80,100,-300,1000} };
+                    move_hvalue = move_wts[child.getMove().getY()][child.getMove().getX()];
+                    
 
-                    n.addChild(getNextStage(child, (myIndex + 1) % 2, currentDepth++, maxDepth));
+                    // Print board
+                    
+                    //java.lang.String game_state_str = child_state.toString();
+                    //System.out.println("---");
+                    //System.out.println(game_state_str);
+                    //System.out.println("---");
+                    
+                   
+                    
+                   //hvalue = 0.01*coinparity_hvalue + 10.0*mobility_hvalue;
+                   //hvalue = position_hvalue;
+                   hvalue = move_hvalue; 
+                   System.out.println(hvalue);
+                   child.setScore(n.getScore()+hvalue);
+
+                   n.addChild(getNextStage(child, (myIndex + 1) % 2, currentDepth++, maxDepth));
                 }
             } 
             else
