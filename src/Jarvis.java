69ed1bc400939a19049f33686c255fdc7de45556
Satish Rajagopal <satishrajagopal.21@gmail.com>
2015-05-16 18:29:04 +0300

    Pushing to branch "Satish"

--- src/Jarvis.java
+++ src/Jarvis.java
@@ -6,10 +6,11 @@ import java.util.Vector;
 
 public class Jarvis implements ReversiAlgorithm
 {
-    // Constants
+	// Initialization
+	
+    //Constants
     private final static int DEPTH_LIMIT = 15;
-
-    // Variables
+    //Variables
     boolean initialized;
     volatile boolean running; // Note: volatile for synchronization issues.
     GameController controller;
@@ -18,6 +19,7 @@ public class Jarvis implements ReversiAlgorithm
     int opIndex;
     Move selectedMove;
     int selectedMoveScore;
+	// score matrix - weights for different positions
     int scoreMatrix[][] = {{10000,-3000,1000,800,800,1000,-3000,10000},
                             {-3000,-5000,-450,-500,-500,-450,-5000,-3000},
                             {1000,-450,30,10,10,30,-450,1000},
@@ -27,30 +29,44 @@ public class Jarvis implements ReversiAlgorithm
                             {-3000,-5000,-450,-500,-500,-450,-5000,-3000},
                             {10000,-3000,1000,800,800,1000,-3000,10000}};
 
-    public Jarvis() {} //the constructor
+	// Constructor
+	
+    public Jarvis() {}
 
+	// Request move
+	
     public void requestMove(GameController requester)
     {
         running = false;
         requester.doMove(selectedMove);
     }
 
+	// Initialization
+	
     public void init(GameController game, GameState state, int playerIndex, int turnLength)
     {
-        initialState = state;
+        initialState = state;	//setting current state to initial state (for depth searching)
         myIndex = playerIndex;
-        opIndex = (playerIndex + 1) % 2;
+        opIndex = (playerIndex + 1) % 2;	//indexes can be only 0 or 1
         controller = game;
         initialized = true;
-        selectedMoveScore = Integer.MIN_VALUE;
+        selectedMoveScore = Integer.MIN_VALUE;	//initial score value
     }
 
+	// Get name of engine
+	
     public String getName() { return "jarvis"; }
 
+	// Cleanup
+	
     public void cleanup() {}
 
-    public void run(){
-        //implementation of the actual algorithm
+	// Run
+	
+    public void run()
+	{
+        //Implementation of the actual algorithm
+		
         while(!initialized);
         initialized = false;
         running = true;
@@ -59,11 +75,12 @@ public class Jarvis implements ReversiAlgorithm
 
         int currentDepth = 1;
 
-        while (running)// && currentDepth < DEPTH_LIMIT)
+        while (running)
         {
+			// Search for next move from tree (alpha-beta pruning)
             Move newMove = searchToDepth(currentDepth++);
 
-            // Check that there's a new move available.
+            // Check if there's a new move available.
             if (newMove != null)
             {
                 selectedMove = newMove;
@@ -80,59 +97,86 @@ public class Jarvis implements ReversiAlgorithm
         }
     }
 
-    private Move searchToDepth(int depth) {
+	// Depth search function
+	
+    private Move searchToDepth(int depth) 
+	{
         //System.out.println(depth);
         selectedMoveScore = Integer.MIN_VALUE;
-        Vector<Move> availableMoves = initialState.getPossibleMoves(myIndex);
+        Vector<Move> availableMoves = initialState.getPossibleMoves(myIndex);	//all possible moves
         int maxScore = Integer.MIN_VALUE;
         int bestIndex = 0;
         if (availableMoves.isEmpty()) return new Move(0,0,-1);
 
-        for (int i = 0; i < availableMoves.size(); ++i) {
+        for (int i = 0; i < availableMoves.size(); ++i) 
+		{
             Move move = availableMoves.elementAt(i);
-            if(checkCorner(move)){
+			
+			// If one of the available moves is corner move, take it.
+			// Because corners are the most strategically important positions
+            if(checkCorner(move))
+			{
                 running = false;
                 return move;
             }
+			
+			// Calculate node score based on Alpha-Beta approach
             int childScore = alphaBetaMinimax(initialState.getNewInstance(move), move, Integer.MIN_VALUE, Integer.MAX_VALUE, 1, opIndex, depth);
-            if(maxScore < childScore){
+            // Comparison with maxscore
+			if(maxScore < childScore)
+			{
+				// @Pablo : Shouldn't we set childscore to maxscore inside this condition
                 bestIndex = i;
             }
         }
         return availableMoves.elementAt(bestIndex);
     }
 
-    private boolean checkCorner(Move move) {
+	// Check corner move function
+	
+    private boolean checkCorner(Move move) 
+	{
         int x  = move.getX();
         int y = move.getY();
         return (x == 0 && y == 0) || (x == 0 && y == 7) || (x == 7 && y == 0) || ( x == 7 && y == 7);
     }
 
-    private int alphaBetaMinimax(GameState state, Move m, int alpha, int beta, int depth, int index, int maxDepth){
-        if(running && depth < maxDepth) {
-            if (beta <= alpha) {
+	// Alpha - Beta function
+	
+	//@Pablo : Copy paste the alpha-beta pseudocode you used here. Maybe add the link as well (if you have)
+	
+    private int alphaBetaMinimax(GameState state, Move m, int alpha, int beta, int depth, int index, int maxDepth)
+	{
+        if(running && depth < maxDepth)
+		{
+            if (beta <= alpha) 
+			{
                 return (index == myIndex) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
 
-            Vector<Move> availableMoves = state.getPossibleMoves(index);
+            Vector<Move> availableMoves = state.getPossibleMoves(index);	//get possible moves from the state
 
-            if (availableMoves.isEmpty()) return 0;
+            if (availableMoves.isEmpty()) return 0;	//no moves left
 
             int maxValue = Integer.MIN_VALUE, minValue = Integer.MAX_VALUE;
 
-            for (int i = 0; i < availableMoves.size(); ++i) {
+            for (int i = 0; i < availableMoves.size(); ++i) 
+			{
                 Move move = availableMoves.elementAt(i);
 
                 int currentScore = 0;
 
-                if (index == myIndex) {
+                if (index == myIndex)
+				{
                     //System.out.println(state.getNewInstance(move).toString() + " " + move.toString() + '\n' + alpha + " " + beta + " " + depth + " " +  opIndex);
                     currentScore = alphaBetaMinimax(state.getNewInstance(move), move,  alpha, beta, depth + 1, opIndex, maxDepth);
                     maxValue = Math.max(maxValue, currentScore);
 
                     //Set alpha
                     alpha = Math.max(currentScore, alpha);
-                } else if (index == opIndex) {
+                } 
+				else if (index == opIndex) 
+				{
 
                     //System.out.println(state.getNewInstance(move) + " " + move.toString() + '\n' + alpha + " " + beta + " " + depth + " " +  myIndex);
                     currentScore = alphaBetaMinimax(state.getNewInstance(move), move, alpha, beta, depth + 1, myIndex, maxDepth);
@@ -141,20 +185,27 @@ public class Jarvis implements ReversiAlgorithm
                     //Set beta
                     beta = Math.min(currentScore, beta);
                 }
-                //reset board
-
+                
                 //If a pruning has been done, don't evaluate the rest of the sibling states
                 if (currentScore == Integer.MAX_VALUE || currentScore == Integer.MIN_VALUE)
                     break;
             }
+			
+			// Return appropriate score based on the player's turn
             return (index == myIndex)?maxValue:minValue;
         }
-        else{
+        else
+		{
+			// Calculate score for move
             return calculateScore(m);
         }
     }
 
-    private int calculateScore(Move m){
+	// Calculate score function
+	
+    private int calculateScore(Move m)
+	{
+		// Return weight of the move position
         return scoreMatrix[m.getX()][m.getY()];
     }
 
